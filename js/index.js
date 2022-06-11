611c0d3fb93f21fa13848e1f1d2b8fd746a57745
Aimee11237 <83794178+Aimee11237@users.noreply.github.com>
2022-06-10 21:14:55 -0400

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value;
+    var pwd=document.getElementById("pwd").value;
+		//修改密码请改此处
+    if(userName=="赵赵" &&  pwd=="0610"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+		}
+	else{
+		alert("提示：用户名两个字，密码四位数");
+	}
+});
